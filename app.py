9b6fce23155e260412ccfde231f908a8900fd5f6
Vinicius <vinicius.pereira@gotobiz.com.br>
2025-03-29 13:50:14 -0300

    feat: application file

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 """
 Just a simple hello-world app.
 """
-
+import os
 from flask import Flask
-
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-    return "Hello, World!"
+    ENV_NAME = os.getenv("ENV_NAME", "World")
+    return "Hello, {}".format(ENV_NAME)
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=8000, debug=True)
